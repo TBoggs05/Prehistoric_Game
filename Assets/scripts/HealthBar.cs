76498b69b220de5284467967bbabda89bf027420
trace boggs <trace.boggs05@gmail.com>
2026-08-16 16:43:10 -0500

    added bars

---
+++ Assets/scripts/HealthBar.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+    public void SetMaxHealth(int max)
+    {
+        slider.maxValue = max;
+        slider.value = max;
+    }
+}
